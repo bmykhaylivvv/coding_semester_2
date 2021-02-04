105c455e5062cd3514fa1cdbb7cf633e6e749d69
Bohdan Mykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-02-05 01:12:54 +0200

    Update setup.py

--- lab_1/puzzle_game/setup.py
+++ lab_1/puzzle_game/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "puzzle_game_bmykhaylivvv",
+    name="puzzle_game_bmykhaylivvv",
     version="0.0.1",
     author="Bohdan Mykhayliv",
     author_email="bohdan.mykhailiv@ucu.edu.ua",
