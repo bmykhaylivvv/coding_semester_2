4ab77705f282bd543cac30892343dc4a8eba9309
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-02-04 20:59:53 +0200

    Almost finished version of "skysparers" task

--- lab_1/skyscapers/skyscrapers.py
+++ lab_1/skyscapers/skyscrapers.py
@@ -1,10 +1,13 @@
+import collections
+
+
 def read_input(path: str):
     """
     Read game board file from path.
     Return list of str.
 
     >>> read_input("check.txt")
-    ['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
+    ['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
     """
 
     with open(path) as brd:
@@ -28,7 +31,18 @@ def left_to_right_check(input_line: str, pivot: int):
     >>> left_to_right_check("452453*", 5)
     False
     """
-    pass
+    counter = 1
+    highest = input_line[1]
+    for i in range(2, len(input_line[1:-1]) + 1):
+        if input_line[i] > highest:
+            counter += 1
+            highest = input_line[i]
+
+
+    if counter == pivot:
+        return True
+
+    return False
 
 
 def check_not_finished_board(board: list):
@@ -44,7 +58,10 @@ def check_not_finished_board(board: list):
     >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***'])
     False
     """
-    pas
+    for line in range(1, len(board)-1):
+        if '?' in board[line][1:-1]:
+            return False
+    return True
 
 
 def check_uniqueness_in_rows(board: list):
@@ -60,7 +77,13 @@ def check_uniqueness_in_rows(board: list):
     >>> check_uniqueness_in_rows(['***21**', '412453*', '423145*', '*553215', '*35214*', '*41532*', '*2*1***'])
     False
     """
-    pass
+    for line in board[1:-1]:
+        dict = collections.Counter(line[1:-1])
+
+        for number in dict.items():
+            if number[1] > 1:
+                return False
+    return True
 
 
 def check_horizontal_visibility(board: list):
@@ -78,7 +101,30 @@ def check_horizontal_visibility(board: list):
     >>> check_horizontal_visibility(['***21**', '452413*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
     False
     """
-    pass
+    def horizontal_visibility(line):
+        if line[0] != '*':
+            pivot = int(line[0])
+            counter = 1
+            highest = line[1]
+            for i in range(2, len(line[1:-1]) + 1):
+                if line[i] > highest:
+                    counter += 1
+                    highest = line[i]
+
+            if counter == pivot:
+                return True
+            return False
+        pass
+
+    board = board + [line[::-1] for line in board]
+
+    tf = []
+    for ln in board:
+        res = horizontal_visibility(ln)
+        if res is not None:
+            tf.append(res)
+
+    return all(tf)
 
 
 def check_columns(board: list):
@@ -94,7 +140,16 @@ def check_columns(board: list):
     >>> check_columns(['***21**', '412553*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
     False
     """
-    pass
+    reversed_board = []
+    for line in range(len(board)):
+        new_line = ''
+        for char in range(len(board[line])):
+            new_line += board[char][line]
+        reversed_board.append(new_line)
+
+    if check_uniqueness_in_rows(reversed_board) and check_horizontal_visibility(reversed_board):
+        return True
+    return False
 
 
 def check_skyscrapers(input_path: str):
@@ -106,8 +161,15 @@ def check_skyscrapers(input_path: str):
     >>> check_skyscrapers("check.txt")
     True
     """
-    pass
+    board = read_input(input_path)
+
+    if check_not_finished_board(board) and check_uniqueness_in_rows(board)\
+         and check_horizontal_visibility(board) and check_columns(board):
+         return True
+    return False
 
 
 if __name__ == "__main__":
     print(check_skyscrapers("check.txt"))
+    import doctest
+    doctest.testmod()
