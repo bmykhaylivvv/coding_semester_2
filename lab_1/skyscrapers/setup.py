fc9e35a1d9653335ed8481688866d3d75981af59
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-02-04 23:16:09 +0200

    Add necessary files and some pylint edits

---
+++ lab_1/skyscrapers/setup.py
@@ -0,0 +1,16 @@
+name = "skyscrapers_project_bmykhaylivvv"
+version = "0.0.1"
+url = "https://github.com/bmykhaylivvv/coding_semester_2/tree/main/lab_1/skyscapers"
+author = "Bohdan Mykhayliv"
+author_email = "bohdan.mykhailiv@ucu.edu.ua"
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
+]
+description = "Project which checks if there is a winning combination \
+    for \"Skyscrapers game\" on the board "
+long_description = "README.md"
+long_description_content_type = "text/markdown"
+
+python_requires = ">=3.6"
