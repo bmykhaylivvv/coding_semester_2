9a4498318d0300497ccb9bc2332dd842477c8e4d
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-02-04 11:24:36 +0200

    Create new project "skysrapers", activate virtual enviroment and write "read_input" function

---
+++ lab_1/skyscapers/for_individual_functions.py
@@ -0,0 +1,15 @@
+def left_to_right_check(input_line: str, pivot: int):
+    """
+    Check row-wise visibility from left to right.
+    Return True if number of building from the left-most hint is visible looking to the right,
+    False otherwise.
+
+    input_line - representing board row.
+    pivot - number on the left-most hint of the input_line.
+
+    >>> left_to_right_check("412453*", 4)
+    True
+    >>> left_to_right_check("452453*", 5)
+    False
+    """
+    pass
