3b5c7496c3c087355f41bf06c39ffc7dc2c48bfc
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-02-05 11:11:10 +0200

    Some edits in puzzle_game

--- lab_1/puzzle_game/puzzle_game.py
+++ lab_1/puzzle_game/puzzle_game.py
@@ -3,7 +3,7 @@ My version of "Puzzle game" board validation on GitHub:
 https://github.com/bmykhaylivvv/coding_semester_2/tree/main/lab_1/puzzle_game
 """
 
-# board for test
+# board for doctests
 board = [
     "**** ****",
     "***1 ****",
@@ -27,6 +27,12 @@ def remove_stars(char):
 def numeric_check(colors):
     """
     Function checks if each element in list is str(number) or blank character
+    >>> numeric_check([[' ', ' ', '3', ' ', ' ', ' ', '2', ' ', ' '],\
+                       [' ', ' ', '6', ' ', ' ', '8', ' ', ' ', '1'],\
+                       [' ', '4', ' ', ' ', ' ', ' ', '2', ' ', ' '],\
+                       ['1', ' ', ' ', ' ', ' ', '8', '2', ' ', ' '],\
+                       [' ', ' ', '3', '1', ' ', '9', ' ', '5', ' ']])
+    True
     """
     numbers_plus_blank = ['1', '2', '3', '4', '5', '6', '7', '8', '9', ' ']
     for color in colors:
@@ -39,6 +45,8 @@ def numeric_check(colors):
 def horizontal_lines(board):
     """
     Function reads horizontal lines on the board
+    >>> horizontal_lines(board)
+    [['*', '*', '*', '*', ' ', '*', '*', '*', '*'], ['*', '*', '*', '1', ' ', '*', '*', '*', '*'], ['*', '*', ' ', ' ', '3', '*', '*', '*', '*'], ['*', ' ', '4', ' ', '1', '*', '*', '*', '*'], [' ', ' ', ' ', ' ', ' ', '9', ' ', '5', ' '], [' ', '6', ' ', ' ', '8', '2', ' ', ' ', '*'], ['3', ' ', ' ', ' ', '2', ' ', ' ', '*', '*'], [' ', ' ', '8', ' ', ' ', '1', '*', '*', '*'], [' ', ' ', '2', ' ', ' ', '*', '*', '*', '*']]
     """
     horizontals = []
     for line in board:
@@ -50,6 +58,8 @@ def horizontal_lines(board):
 def vertical_lines(board):
     """
     Function reads vertical lines on the board
+    >>> vertical_lines(board)
+    [['*', '*', '*', '*', ' ', ' ', '3', ' ', ' '], ['*', '*', '*', ' ', ' ', '6', ' ', ' ', ' '], ['*', '*', ' ', '4', ' ', ' ', ' ', '8', '2'], ['*', '1', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', '3', '1', ' ', '8', '2', ' ', ' '], ['*', '*', '*', '*', '9', '2', ' ', '1', '*'], ['*', '*', '*', '*', ' ', ' ', ' ', '*', '*'], ['*', '*', '*', '*', '5', ' ', '*', '*', '*'], ['*', '*', '*', '*', ' ', '*', '*', '*', '*']]
     """
     verticals = []
     column = []
@@ -64,6 +74,8 @@ def vertical_lines(board):
 def one_color(board):
     """
     Function reads sections with the same color on the boards
+    >>> one_color(board)
+    [[' ', ' ', '3', ' ', ' ', ' ', '2', ' ', ' '], [' ', ' ', '6', ' ', ' ', '8', ' ', ' ', '1'], [' ', '4', ' ', ' ', ' ', ' ', '2', ' ', ' '], ['1', ' ', ' ', ' ', ' ', '8', '2', ' ', ' '], [' ', ' ', '3', '1', ' ', '9', ' ', '5', ' ']]
     """
     same_color = []
     vertical = []
@@ -84,6 +96,16 @@ def repeat_check(lst):
     """
     Fuction checks if there are repeatable characters in rows, columns or 
     sections with the same color
+    >>> repeat_check([['*', '*', '*', '*', ' ', ' ', '3', ' ', ' '],\
+                      ['*', '*', '*', ' ', ' ', '6', ' ', ' ', ' '],\
+                      ['*', '*', ' ', '4', ' ', ' ', ' ', '8', '2'],\
+                      ['*', '1', ' ', ' ', ' ', ' ', ' ', ' ', ' '],\
+                      [' ', ' ', '3', '1', ' ', '8', '2', ' ', ' '],\
+                      ['*', '*', '*', '*', '9', '2', ' ', '1', '*'],\
+                      ['*', '*', '*', '*', ' ', ' ', ' ', '*', '*'],\
+                      ['*', '*', '*', '*', '5', ' ', '*', '*', '*'],\
+                      ['*', '*', '*', '*', ' ', '*', '*', '*', '*']])
+    True
     """
     new_lines = []
     for line in lst:
@@ -100,9 +122,12 @@ def validate_board(board):
     """
     MAIN FUNCTION
     Function checks if board is valid for game
+    >>> validate_board(board)
+    True
     """
+
     if repeat_check(horizontal_lines(board)) and repeat_check(vertical_lines(board))\
-            and repeat_check(one_color(board)) and numeric_check(one_color(board)):
+            and repeat_check(one_color(board)+vertical_lines(board)+horizontal_lines(board)) and numeric_check(one_color(board)):
         return True
     return False
 
--- lab_1/skyscrapers/setup.py
+++ lab_1/skyscrapers/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
+b
 setuptools.setup(
     name="skyscrapers_project_bmykhaylivvv",
     version="0.0.1",
